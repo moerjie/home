0ce40da9cb58fb5e78f88647ed0dfe8d0381af4c
moerjie <3165109406@qq.com>
2024-11-23 12:56:09 +0800

    Add gaode weather

--- src/api/index.js
+++ src/api/index.js
@@ -62,7 +62,7 @@ export const getAdcode = async (key) => {
 // 获取高德地理天气信息
 export const getWeather = async (key, city) => {
   const res = await fetch(
-    `https://restapi.amap.com/v3/weather/weatherInfo?key=${a5950e95dbdb581250a3ac5a37754c7e}&city=${110105}`,
+    `https://restapi.amap.com/v3/weather/weatherInfo?key=${a5950e95dbdb581250a3ac5a37754c7e}&city=${010}`,
   );
   return await res.json();
 };
